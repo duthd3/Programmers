7d507ab842c2651169366b8034207ca1add81e17
YeoSong <duthd3@naver.com>
2023-02-17 23:16:51 +0900

    Create 피자 나눠먹기1.swift

---
+++ 코딩테스트입문/피자 나눠먹기1.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var answer = 0
+    if n%7 == 0 {
+        answer = n/7
+    } else{
+        answer = n/7 + 1
+    }
+    return answer
+}
