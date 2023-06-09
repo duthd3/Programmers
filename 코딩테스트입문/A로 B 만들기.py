83c9bddfa9423bb4f3f2225f83473f656f50170d
YeoSong <duthd3@naver.com>
2023-06-10 01:43:33 +0900

    Create A로 B 만들기.py

---
+++ 코딩테스트입문/A로 B 만들기.py
@@ -0,0 +1,10 @@
+def solution(before, after):
+    before = sorted(before)
+    after = sorted(after)
+    if before == after :
+        return 1
+    else:
+        return 0
+               
+    
+    
