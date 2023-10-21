dc2b0fc0d4c8d00e0a4cf7388a3fdec4b4918f95
YeoSong <duthd3@naver.com>
2023-10-21 20:47:34 +0900

    Create K번째 수.py

---
+++ LV1/K번째 수.py
@@ -0,0 +1,23 @@
+def solution(array, commands):
+    answer = []
+    
+    for i in range(len(commands)):
+        
+    
+        a = array[(commands[i][0] - 1) : (commands[i][1])]
+        a.sort()
+     
+        answer.append(a[commands[i][2] -1])
+    
+    return answerdef solution(array, commands):
+    answer = []
+    
+    for i in range(len(commands)):
+        
+    
+        a = array[(commands[i][0] - 1) : (commands[i][1])]
+        a.sort()
+     
+        answer.append(a[commands[i][2] -1])
+    
+    return answer
